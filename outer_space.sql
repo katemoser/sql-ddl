72534eaee2b6bac67548b75c4124c745e8263a02
Kate Moser <komoser@gmail.com>
2022-02-01 18:16:06 -0600

    finished outerspace schema

--- outer_space.sql
+++ outer_space.sql
@@ -1,19 +1,17 @@
 CREATE TABLE planets (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    orbital_period NUMERIC(10, 3)
-)
+    orbital_period NUMERIC(10, 3),
+    star_id INT REFERENCES stars;)
 
 
 CREATE TABLE stars (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    temp INT
-)
-
+    temp INT);
 
 
 CREATE TABLE moons (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-)
+    name VARCHAR(50) NOT NULL,
+    planet_id INT NOT NULL REFERENCES planets);
